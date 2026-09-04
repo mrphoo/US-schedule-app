ddc5bf8b6fd67a41402f67b6f6bfd27d410f1d0a
mrphoo <ns25279tj@isk.ed.jp>
2026-09-04 14:57:39 +0900

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,11 +3,11 @@
 // ========================================
 
 // SupabaseのProject URL
-const SUPABASE_URL = 'https://YOUR-PROJECT-ID.supabase.co';
+const SUPABASE_URL = 'https://andeaxxmxttboxwwnbhb.supabase.co';
 
 // SupabaseのPublishable key
 // ※ service_role key は絶対に入れない
-const SUPABASE_PUBLISHABLE_KEY = 'YOUR-PUBLISHABLE-KEY';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable__7AkHbUCWEpU2wHJgfytrg_lz2Xb4RR';
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
